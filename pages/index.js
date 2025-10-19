edce72414f32e99d3292a51ed47b77f5182ca170
Vinicius Sakamoto <viniciuskosakamoto@gmail.com>
2025-10-19 11:44:55 -0300

    feat: Pag em construcao

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import styles from "./Home.module.css";
+
 function Home() {
-  return <h1>Voltando a estudar!! Agora!!</h1>;
+  return (
+    <div className={styles.homeContainer}>
+      <h1>Voltando a estudar!! Agora!!</h1>
+      <h2>Página em construção!!</h2>
+      <p>Página voltada para treinar programação!!</p>
+    </div>
+  );
 }
 
 export default Home;
